698943b00ca245eed63faf41d98fee9d37631bd4
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-05-30 19:30:44 -0300

    conectando a url do render

--- src/db/index.js
+++ src/db/index.js
@@ -13,6 +13,24 @@ sequelize.authenticate()
         console.log(`Erro ao se conectar: ${err}`);
     });
 
+
+const { Client } = require("pg");
+
+
+const connectionString = "postgres://mikael:35mnFkyn2wTXkMFiXjfbt2JpmdRbchWy@dpg-chr77f9mbg5e1f0g47tg-a.oregon-postgres.render.com/users_wwzx";
+const client = new Client({
+    connectionString: connectionString
+});
+
+client.connect()
+    .then(() => {
+        console.log("Conexão bem-sucedida ao banco de dados!");
+        // Realize operações no banco de dados
+    })
+    .catch((err) => {
+        console.error("Erro ao conectar ao banco de dados:", err);
+    });
+
 module.exports = {
     Sequelize: Sequelize,
     sequelize: sequelize
