4bba467c70a18af39e4bd4dc96290e870408cc27
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-05-30 15:01:30 -0300

    Configurando o CORS

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const User = require("./models/user");
+const cors = require("cors");
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.get("/", (_, res) => {
+    res.status(200).send("OK");
+});
+
+
 
 app.listen(PORT, () => console.log("http://localhost:3000"));
