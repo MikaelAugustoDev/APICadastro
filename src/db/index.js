19ff658db97c7484aee90be60390eb8451f0056b
lucaoskaique <lucas.kaique175@hotmail.com>
2023-06-02 21:09:44 -0300

    fix db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,18 @@
 const { Pool } = require("pg");
 
-const dbConfig = {
-    user: "mikael",
-    host: "dpg-chr77f9mbg5e1f0g47tg-a",
-    database: "users_wwzx",
-    password: "35mnFkyn2wTXkMFiXjfbt2JpmdRbchWy",
-    port: 5432,
-};
+// const dbConfig = {
+//     user: "mikael",
+//     host: "dpg-chr77f9mbg5e1f0g47tg-a",
+//     database: "users_wwzx",
+//     password: "35mnFkyn2wTXkMFiXjfbt2JpmdRbchWy",
+//     port: 5432,
+// };
 
-const pooli = new Pool(dbConfig);
+const poll = new Pool({
+    connectionString: "postgres://mikael:35mnFkyn2wTXkMFiXjfbt2JpmdRbchWy@dpg-chr77f9mbg5e1f0g47tg-a.oregon-postgres.render.com/users_wwzx",
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
-pooli.connect()
-    .then(() => {
-        console.log("Conexão bem-sucedida ao banco de dados!");
-        // Realize operações no banco de dados
-    })
-    .catch((err) => {
-        console.error("Erro ao conectar ao banco de dados:", err);
-    });
-
-module.exports = pooli;
+module.exports = poll;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const { Pool } = require("pg");
-const pooli = require("./db");
-
-const dbUrl = "postgres://mikael:35mnFkyn2wTXkMFiXjfbt2JpmdRbchWy@dpg-chr77f9mbg5e1f0g47tg-a.oregon-postgres.render.com/users_wwzx";
-
-const pool = new Pool({
-    connectionString: dbUrl,
-});
+const pool = require("./db");
 
 const PORT = 3000;
 const app = express();
@@ -17,18 +10,18 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
-    res.status(200).json({msg: "Home"});
+    res.status(200).json({ msg: "Home" });
 });
 
 app.post("/cadastro", async (req, res) => {
-    const {email, senha} = req.body;
+    const { email, senha } = req.body;
 
-    if(!email) {
-        return res.status(404).json({msg: "O email é obrigatorio"});
+    if (!email) {
+        return res.status(404).json({ msg: "O email é obrigatorio" });
     }
 
-    if(!senha) {
-        return res.status(404).json({msg: "A senha é obrigatoria"});
+    if (!senha) {
+        return res.status(404).json({ msg: "A senha é obrigatoria" });
     }
 
     try {
@@ -36,7 +29,7 @@ app.post("/cadastro", async (req, res) => {
         const result = await pool.query(query, [email]);
 
         if (result.rows.length > 0) {
-            return res.status(404).json({msg: "Email já cadastrado"}); 
+            return res.status(404).json({ msg: "Email já cadastrado" });
         }
 
         const salt = await bcrypt.genSalt(12);
@@ -45,22 +38,22 @@ app.post("/cadastro", async (req, res) => {
         const insertQuery = "INSERT INTO users (email, senha) VALUES ($1, $2)";
         await pool.query(insertQuery, [email, senhaHash]);
 
-        res.status(201).json({msg: "Usuario cadastrado com sucesso"});
-    } catch(err) {
+        res.status(201).json({ msg: "Usuario cadastrado com sucesso" });
+    } catch (err) {
         console.log(err);
-        res.status(500).json({msg: "erro"});
+        res.status(500).json({ msg: "erro" });
     }
 });
 
 app.post("/login", async (req, res) => {
-    const {email, senha} = req.body;
+    const { email, senha } = req.body;
 
-    if(!email) {
-        return res.status(404).json({msg: "O email é obrigatorio"});
+    if (!email) {
+        return res.status(404).json({ msg: "O email é obrigatorio" });
     }
 
-    if(!senha) {
-        return res.status(404).json({msg: "A senha é obrigatoria"});
+    if (!senha) {
+        return res.status(404).json({ msg: "A senha é obrigatoria" });
     }
 
     try {
@@ -68,21 +61,23 @@ app.post("/login", async (req, res) => {
         const result = await pool.query(query, [email]);
 
         if (result.rows.length === 0) {
-            return res.status(404).json({msg: "Usuario não encontrado"}); 
+            return res.status(404).json({ msg: "Usuario não encontrado" });
         }
 
         const user = result.rows[0];
         const checksenha = await bcrypt.compare(senha, user.senha);
 
-        if(!checksenha) {
-            return res.status(404).json({msg: "Senha invalida"});
+        if (!checksenha) {
+            return res.status(404).json({ msg: "Senha invalida" });
         }
 
-        return res.status(200).json({msg: "Usuario logado com sucesso"});
-    } catch(err) {
+        return res.status(200).json({ msg: "Usuario logado com sucesso" });
+    } catch (err) {
         console.log(err);
-        res.status(500).json({msg: "erro"});
+        res.status(500).json({ msg: "erro" });
     }
 });
 
-app.listen(PORT, () => console.log(`Server rodando em http://localhost:${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Server rodando em http://localhost:${PORT}`)
+);
